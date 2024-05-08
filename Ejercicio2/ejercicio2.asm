4f27cb79a245b954c705808ee42dd0f99d1db574
Javier Peraza <contacto.peraza@gmail.com>
2024-05-07 21:56:15 -0600

    resuelto ejercicio 2

--- Ejercicio2/ejercicio2.asm
+++ Ejercicio2/ejercicio2.asm
@@ -1,37 +1,44 @@
 section .data
-    ; Mensaje de encabezado para el resultado
-    msj_resultado db 'El resultado de la multiplicacion de: ', 0
-    result_len equ $ - msj_resultado  ; Longitud del mensaje de encabezado
-    newline db 10                      ; Salto de línea
-
-section .bss
-    resultado resb 1
+    numero1 db 2     
+    numero2 db 2     
+    resultado db 0   
+    resultado_msg db "El resultado de 2 * 2 es: ", 0 ; Mensaje a mostrar
+    nueva_linea db 0xA 
+    result_len equ $ - resultado_msg
 
 section .text
-global _start
+    global _start
 
 _start:
-mov ax, 3
-mov cx, 2
+    ; Multiplicar los números
+    mov al, [numero1]  ; Mover el primer número a AL
+    mov bl, [numero2]  ; Mover el segundo número a BL
+    mul bl             ; Multiplicar AL por BL
+    add al, '0'        ; Convertir el resultado a ASCII
+    mov [resultado], al ; Almacenar el resultado en la variable 'resultado'
 
-mul cx
+    ; Mostrar el mensaje en la consola
+    mov eax, 4              
+    mov ebx, 1              
+    mov ecx, resultado_msg  
+    mov edx, result_len       
+    int 0x80                
 
-add ax, 48
-mov [resultado], ax
+    ; Mostrar el resultado en la consola
+    mov eax, 4              
+    mov ebx, 1              
+    mov ecx, resultado      
+    mov edx, 1              
+    int 0x80                
 
-; Mostrar el mensaje de resultado
-mov eax, 4
-mov ebx, 1
-mov ecx, msj_resultado
-mov edx, result_len
-int 0x80
+    ; Mostrar el carácter de nueva línea
+    mov eax, 4              
+    mov ebx, 1              
+    mov ecx, nueva_linea    
+    mov edx, 1              
+    int 0x80                
 
-mov eax, 4
-mov ebx, 1
-mov ecx, resultado
-mov edx, 1
-int 0x80
-
-mov eax, 1
-mov ebx, 0
-int 0x80
+    ; Salir del programa
+    mov eax, 1       
+    xor ebx, ebx     
+    int 0x80         
